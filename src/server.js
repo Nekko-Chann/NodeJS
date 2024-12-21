32f9cc4c997afba37c42dd6df1a25f68488d94be
Neko Chan <nekkochan2k@gmail.com>
2024-12-22 03:59:39 +0700

    Static File

--- src/server.js
+++ src/server.js
@@ -3,14 +3,15 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World with Edan Nguyen');
 });
 
 app.get('/edan', (req, res) => {
