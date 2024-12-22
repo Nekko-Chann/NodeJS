bdf1ab640086f653edd7a559d3102c02a7bed4d6
Neko Chan <nekkochan2k@gmail.com>
2024-12-23 02:45:16 +0700

    Design NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,7 @@
 const connection = require("../config/database");
 
 const HomePage = (req, res) => {
-    let user = [];
-    connection.query(
-        'SELECT * FROM users',
-        function (err, results, fields) {
-            user = results;
-            console.log("Check results = ", results);
-            res.send(JSON.stringify(user));
-        }
-    )
-
+    return res.render('home');
 }
 
 const EdanPage = (req, res) => {
