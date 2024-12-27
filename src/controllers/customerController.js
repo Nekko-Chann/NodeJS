ac0a00de1504bed51a9cdaca1c6338e6570dce27
Neko Chan <nekkochan2k@gmail.com>
2024-12-28 01:38:19 +0700

    Validate Data

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -3,9 +3,9 @@ const {
     getAllCustomer, createCustomer, createArrayCustomer,
     updateCustomer, deleteCustomer, deleteArrayCustomer
 } = require("../services/customerService");
+const Joi = require('joi');
 
 const GetCustomersAPI = async (req, res) => {
-
     let {limit, page, name} = req.query;
     let result = null;
     if (limit && page) {
@@ -21,20 +21,35 @@ const GetCustomersAPI = async (req, res) => {
 
 const CreateCustomersAPI = async (req, res) => {
     let {name, address, phone, email, description} = req.body;
-
-    let imageURL = '';
-    if (!req.files || Object.keys(req.files).length === 0) {
-        //do nothing
+    const schema = Joi.object({
+        name: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required(),
+        address: Joi.string(),
+        phone: Joi.string().pattern(new RegExp('^[0-9]{8,11}$')),
+        email: Joi.string().email(),
+        description: Joi.string(),
+    })
+    const {error} = schema.validate(req.body);
+    if (error) {
+        //return error
     } else {
-        let result = await uploadSingleFile(req.files.image);
-        imageURL = result.path;
+        let imageURL = '';
+        if (!req.files || Object.keys(req.files).length === 0) {
+            //do nothing
+        } else {
+            let result = await uploadSingleFile(req.files.image);
+            imageURL = result.path;
+        }
+        let customerData = {name, address, phone, email, description, image: imageURL};
+        let customer = await createCustomer(customerData);
+        return res.status(200).json({
+            errorCode: 0,
+            data: customer,
+        });
     }
-    let customerData = {name, address, phone, email, description, image: imageURL};
-    let customer = await createCustomer(customerData);
-    return res.status(200).json({
-        errorCode: 0,
-        data: customer,
-    });
 }
 
 const CreateManyCustomersAPI = async (req, res) => {
--- src/services/productService.js
+++ src/services/productService.js
@@ -26,6 +26,13 @@ const createProject = async (data) => {
 
             return await myProject.save();
         }
+        if (data.type === "ADD-TASKS") {
+            let myProject = await Project.findById(data.projectId).exec();
+            for (let i = 0; i < data.taskArr.length; i++) {
+                myProject.tasks.push(data.taskArr[i]);
+            }
+            return await myProject.save();
+        }
         if (data.type === "REMOVE-USERS") {
             let myProject = await Project.findById(data.projectId).exec();
             for (let i = 0; i < data.usersArr.length; i++) {
